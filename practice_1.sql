189515eabc8f308a636c5d8248af19d53a60ef16
Sam-Sam <sammyguyen1804@gmail.com>
2024-05-28 22:15:26 +1000

    Update practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -3,3 +3,13 @@ SELECT product_id  FROM Products
 WHERE low_fats = "Y"
 AND recyclable = "Y"
 
+--EX11
+SELECT name, population, area FROM WORLD
+WHERE population >= 25000000
+OR area >= 3000000;
+
+--EX10
+SELECT  name FROM Customer 
+WHERE  referee_id != 2
+OR referee_id IS NULL
+
