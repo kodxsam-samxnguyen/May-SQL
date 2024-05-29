eb6e230b919aa10c7c0155ca6df3c9f8bf83f43b
Sam-Sam <sammyguyen1804@gmail.com>
2024-05-29 21:46:29 +1000

    Update practice_1.sql

--- practice_1.sql
+++ practice_1.sql
@@ -17,3 +17,9 @@ OR referee_id IS NULL
 SELECT DISTINCT author_id AS id FROM Views
 WHERE author_id = viewer_id
 ORDER BY id
+
+--EX13
+SELECT part, assembly_step  FROM parts_assembly
+WHERE finish_date IS NULL
+ORDER BY assembly_step
+
