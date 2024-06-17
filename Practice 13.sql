166f08cf969ca501f7a727f055c785ec1d3ddeab
Sam-Sam <sammyguyen1804@gmail.com>
2024-06-17 18:46:04 +1000

    Update Practice 13.sql

--- Practice 13.sql
+++ Practice 13.sql
@@ -30,3 +30,20 @@ FROM pages
 LEFT JOIN page_likes ON pages.page_id = page_likes.page_id
 WHERE page_likes.page_id is null
 ORDER BY page_id ASC;
+
+--ex 6
+SELECT  SUBSTR(trans_date,1,7) as month,
+ country, count(id) as trans_count, 
+ SUM(CASE WHEN state = 'approved' then 1 else 0 END) as approved_count,
+ SUM(amount) as trans_total_amount, SUM(CASE WHEN state = 'approved' then amount else 0 END) as approved_total_amount
+FROM Transactions
+GROUP BY month, country;
+
+--EX 7
+SELECT product_id, year AS first_year, quantity, price 
+FROM Sales 
+WHERE (product_id, year) IN (
+SELECT product_id, min(year) AS year 
+FROM Sales 
+GROUP BY product_id 
+);
